efd9c349646d64374136bc2d7360c571574807bc
NguyenXuanAn <nguyenxuanan.nxa@gmail.com>
2019-10-17 23:53:44 +0700

    Add project files.

---
+++ DaNenTang_Tuan2/DaNenTang_Tuan2/Bai1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DaNenTang_Tuan2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Bai1 : ContentPage
+    {
+        System.Collections.ObjectModel.ObservableCollection<Hoa> Hoas = new System.Collections.ObjectModel.ObservableCollection<Hoa>();
+        public Bai1()
+        {
+            InitializeComponent();
+            Hoas.Add(new Hoa { TenHoa = "Đón xuân", Gia = 50000, hinh = "cuc_9.jpg", MoTa = "Hoa cúc các màu: trắng, vàng, cam" });
+            lsvHoa.ItemsSource = Hoas;
+        }
+    }
+}
---
+++ DaNenTang_Tuan2/DaNenTang_Tuan2/Hoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaNenTang_Tuan2
+{
+    class Hoa
+    {
+        public string TenHoa{ get; set; }
+        public string hinh { get; set; }
+        public double Gia { get; set; }
+        public string MoTa { get; set; }
+    }
+}
